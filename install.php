784030b7bcab6dac39b45da715edb6a0a0ea5f83
Michael Snoeren <MSnoeren1995@gmail.com>
2020-02-18 09:28:41 +0000

    Apply fixes from StyleCI

--- install.php
+++ install.php
@@ -1,8 +1,10 @@
 <?php
-declare(strict_types = 1);
+
+declare(strict_types=1);
 
 /**
- * R2H License Plugin
+ * R2H License Plugin.
+ *
  * @author      Michael Snoeren <michael@r2h.nl>
  * @copyright   R2H Marketing & Internet Solutions © 2019
  * @license     GNU/GPLv3
@@ -16,16 +18,16 @@ class PlgSystemR2HLicenseInstallerScript
 {
     /**
      * Enable the plugin after installation and put the license key in the right directory if found.
-     * @access  public
-     * @return  boolean
+     *
+     * @return bool
      */
     public function postflight()
     {
         jimport('joomla.filesystem.file');
 
         // Check if the key exists and if so, move it aswell.
-        $currentLocation = __DIR__ . '/license.key';
-        $targetLocation = JPATH_ROOT . '/plugins/system/r2hlicense/license.key';
+        $currentLocation = __DIR__.'/license.key';
+        $targetLocation = JPATH_ROOT.'/plugins/system/r2hlicense/license.key';
 
         if (JFile::exists($currentLocation)) {
             JFile::move($currentLocation, $targetLocation);
@@ -37,13 +39,14 @@ class PlgSystemR2HLicenseInstallerScript
 
         $query
         ->update('#__extensions')
-        ->set($db->qn('enabled') . ' = 1')
+        ->set($db->qn('enabled').' = 1')
         ->where([
-            $db->qn('type') . ' = ' . $db->q('plugin'),
-            $db->qn('element') . ' = ' . $db->q('r2hlicense'),
+            $db->qn('type').' = '.$db->q('plugin'),
+            $db->qn('element').' = '.$db->q('r2hlicense'),
         ]);
 
         $db->setQuery($query);
+
         return (bool) $db->execute();
     }
 }
